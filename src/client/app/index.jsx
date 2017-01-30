9df8cee1444597cf270f64fae3f355e05a42df0d
Johnny <po_bo@hotmail.com>
2017-01-30 13:48:04 -0800

    added search box handler and click handler function, extended App to state full, also changed {render} to reactDOM for easy understanding, added live server for dev dependencies

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import Nav from './Nav.jsx'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      studentList: [],
+      studentPhoto: '',
+      searchInput: ''
+    };
+
+    //binding all the method to this context before pass down to components.
+    this.handleChangeSearch = this.handleChangeSearch.bind(this);
+    this.searchClicked = this.searchClicked.bind(this);
+  };
+
+  //create handler method to extract search input box value
+  handleChangeSearch (e) {
+    this.setState({
+      searchInput: e.target.value
+    });
+    console.log(this.state.searchInput)
+  };
+
+  //create handler method for search button clicked
+  searchClicked (e) {
+    console.log(this.state.searchInput)
+  }
+
+
+
+
+
   render () {
     return (
       <div>
-        <Nav />
+        <Nav searchText={this.handleChangeSearch} searchClicked={this.searchClicked} />
       </div>
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
