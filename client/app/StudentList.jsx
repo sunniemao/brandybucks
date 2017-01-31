69d15ebc44c10375fa0a86ba50d829be700ca88b
Sunnie Mao <mao.sunnie@gmail.com>
2017-01-30 18:54:59 -0800

    make style changes to search input

--- client/app/StudentList.jsx
+++ client/app/StudentList.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const StudentList = (props) => {
   return(
-    <div>
-    hi!
+      <div id="wrapper">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-12">
+                    Students Students Students Students Students Students Students 
+                    Students Students Students Students Students Students Students Students 
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
