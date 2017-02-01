9a2adcc4cd4927db8d053700799581278d45df0f
Sunnie Mao <mao.sunnie@gmail.com>
2017-02-01 09:44:20 -0800

    start login/signup pages

--- client/app/Signup.jsx
+++ client/app/Signup.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class Login extends React.Component {
+class Signup extends React.Component {
   render () {
     return (
       <div id="wrapper">
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
-                  <h1>Login</h1>
+                  <h1>Sign Up</h1>
             </div>
           </div>
           <div className="row">
             <div className="col-md-12">
-            <form action="/login" method="post">
+            <form action="/signup" method="post">
                   <b>Username:</b> &nbsp;
                   <input id="username" type="text" name="username" /> &nbsp;
                   <b>Password:</b> &nbsp;
                   <input id="password" type="password" name="password" /> &nbsp;
-                  <input type="submit" className="btn login-btn" value="&nbsp;Login&nbsp;" />
+                  <input type="submit" className="btn login-btn" value="&nbsp;Sign Up&nbsp;" />
             </form>
             <p />
-              <Link to="/signup">Create an Account &rarr;</Link>
+              <Link to="/login">Already have an account? Login &rarr;</Link>
             <p />
             </div>
           </div>
@@ -31,4 +31,4 @@ class Login extends React.Component {
   }
 }
 
-export {Login};
+export {Signup};
