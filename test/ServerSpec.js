6f365a58a94ba82c84ba72c4a447f9e90d6b5694
Tony Lopes <tony@lopesdesign.com>
2017-01-31 20:46:30 -0800

    adds another test

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -23,4 +23,22 @@ describe('server', function() {
       done();
     });
   });
+
+  it('should not respond with "Cannot GET" at any unrecognized route', function(done) {
+    request('http://127.0.0.1:4570/OneLayerRoute', function(error, response, body) {
+      // console.log(response.body.slice(0, 10));
+      expect(response.body.slice(0, 10)).to.not.equal('Cannot GET');
+      done();
+    });
+  })
+
+  it('should not respond with "Cannot GET" at any unrecognized route, nested routes', function(done) {
+    request('http://127.0.0.1:4570/OneLayerRoute/two/three/four', function(error, response, body) {
+      // console.log(response.body.slice(0, 10));
+      expect(response.body.slice(0, 10)).to.not.equal('Cannot GET');
+      done();
+    });
+  })
 });
+
+
